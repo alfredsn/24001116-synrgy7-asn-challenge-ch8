a82efd0fe5770f9852799cea4cd116a6dff0cb5b
Alfred Sahala Nainggolan <alfred.nainggolan00@gmail.com>
2024-07-01 14:46:46 +0700

    update deploy,yml

--- data/src/main/java/com/example/data/datasource/remote/ClientApi.kt
+++ data/src/main/java/com/example/data/datasource/remote/ClientApi.kt
@@ -1,37 +1,17 @@
 package com.example.data.datasource.remote
 
-import android.content.Context
-import com.chuckerteam.chucker.api.ChuckerCollector
-import com.chuckerteam.chucker.api.ChuckerInterceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ClientApi {
-    private lateinit var retrofit: Retrofit
+    private val client = OkHttpClient.Builder().build()
 
-    fun init(context: Context) {
-        val okHttpClient = createOkHttpClient(context)
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://mail-sender-api1.p.rapidapi.com/")
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
-        retrofit = Retrofit.Builder()
-            .baseUrl("https://mail-sender-api1.p.rapidapi.com/")
-            .client(okHttpClient)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
-
-    private fun createOkHttpClient(context: Context): OkHttpClient {
-        val chuckerInterceptor = ChuckerInterceptor.Builder(context)
-            .collector(ChuckerCollector(context))
-            .maxContentLength(250_000L)
-            .redactHeaders(emptySet())
-            .alwaysReadResponseBody(false)
-            .build()
-
-        return OkHttpClient.Builder()
-            .addInterceptor(chuckerInterceptor)
-            .build()
-    }
-
-    val api: ServiceApi by lazy { retrofit.create(ServiceApi::class.java) }
+    val api: ServiceApi = retrofit.create(ServiceApi::class.java)
 }
